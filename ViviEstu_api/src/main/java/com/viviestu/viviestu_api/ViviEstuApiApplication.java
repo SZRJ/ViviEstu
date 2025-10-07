6ce6455a9c5d6e16eaa76698a026c79e4a615a17
Nisijou <62604958+SZRJ@users.noreply.github.com>
2025-10-07 08:34:09 -0500

    Base

---
+++ ViviEstu_api/src/main/java/com/viviestu/viviestu_api/ViviEstuApiApplication.java
@@ -0,0 +1,13 @@
+package com.viviestu.viviestu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViviEstuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViviEstuApiApplication.class, args);
+    }
+
+}
